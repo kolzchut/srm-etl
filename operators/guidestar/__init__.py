e1c7db9ee2e83d6a3fdde6dfefcc662333f1c029
Adam Kariv <adam.kariv@gmail.com>
2021-06-27 13:40:04 +0300

    Missing operators directory

---
+++ operators/guidestar/__init__.py
@@ -0,0 +1,9 @@
+import dataflows as DF
+import logging
+
+def operator(name, params, pipeline):
+    logging.info('STARTING Guidestar Scraping')
+
+
+if __name__ == '__main__':
+    operator(None, None, None)
