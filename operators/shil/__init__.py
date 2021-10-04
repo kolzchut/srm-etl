ad6da48be6f42c422f6dc4e3df7b2ef9c1d0d0cc
Paul Walsh <paul@walsh.co.il>
2021-10-04 13:38:12 +0300

    Workarounds for govapi.

--- operators/shil/__init__.py
+++ operators/shil/__init__.py
@@ -1,8 +1,8 @@
 import bleach
 import dataflows as DF
-import requests
 
 from conf import settings
+from srm_tools.gov import get_gov_api
 from srm_tools.logger import logger
 from srm_tools.update_table import airflow_table_updater
 
@@ -58,18 +58,16 @@ FIELD_MAP = {
         # thought it should be an array but doesnt look right in airtable.
         # cant clearly see in code how we delimit multiple, so going for comma here.
         'type': 'string',
-        'transform': lambda r: ','.join(
-            filter(None, [r['PhoneNumber'], r['PhoneNumber2']])
-        ),
+        'transform': lambda r: ','.join(filter(None, [r['PhoneNumber'], r['PhoneNumber2']])),
     },
     'address_details': {
         'source': 'Location',
     },
     'description': {
         'source': 'Description',
-        'transform': lambda r: bleach.clean(
-            r['Description'], tags=tuple(), strip=True
-        ).replace('&nbsp;', ' '),
+        'transform': lambda r: bleach.clean(r['Description'], tags=tuple(), strip=True).replace(
+            '&nbsp;', ' '
+        ),
     },
     # TODO - shouldnt we store emails?
     # 'emails': {'source': 'Email', 'type': 'array', 'transform': lambda r: [r['Email']]},
@@ -106,29 +104,14 @@ def ensure_field(name, args, resources=None):
 
 
 def get_shil_data():
-    # Note: The gov API is buggy or just weird. It looks like you can set a high limit of items,
-    # but the most that get returned in any payload is 50.
-    # If you pass, for example, limit 1000, the stats on the response object will say:
-    # {... "total":90,"start_index":0,"page_size":1000,"current_page":1,"pages":1...}
-    # but this is wrong - you only have 50 items, and it turns out you need to iterate by using
-    # skip. And then, the interaction between limit and skip is weird to me.
-    # you need to set a limit higher than the results we will retreive, but whatever you put in limit
-    # is used, minus start_index, to declare page_size, which is wrong ......
-    # we are going to batch in 50s which seems to be the upper limit for a result set.
     skip = 0
     skip_by = 50
-    timeout = 5
-    response = requests.get(
-        settings.SHIL_API, params={'limit': 1000, 'skip': skip}, timeout=timeout
-    ).json()
-    total = response['total']
-    results = response['results']
+    total, results = get_gov_api(settings.SHIL_API, skip)
+
     while len(results) < total:
         skip += skip + skip_by
-        response = requests.get(
-            settings.SHIL_API, params={'limit': 1000, 'skip': skip}, timeout=timeout
-        ).json()
-        results.extend(response['results'])
+        _, batch = get_gov_api(settings.SHIL_API, skip)
+        results.extend(batch)
     return results
 
 
@@ -160,17 +143,12 @@ def shil_branch_data_flow():
         DF.Flow(
             get_shil_data(),
             DF.update_resource(name='branches', path='branches.csv', resources=-1),
-            *[
-                ensure_field(key, args, resources=['branches'])
-                for key, args in FIELD_MAP.items()
-            ],
+            *[ensure_field(key, args, resources=['branches']) for key, args in FIELD_MAP.items()],
             DF.select_fields(list(FIELD_MAP.keys()), resources=['branches']),
             DF.add_field(
                 'data',
                 'object',
-                lambda r: {
-                    k: v for k, v in r.items() if not k in ('id', 'source', 'status')
-                },
+                lambda r: {k: v for k, v in r.items() if not k in ('id', 'source', 'status')},
                 resources=['branches'],
             ),
             DF.select_fields(['id', 'data'], resources=['branches']),
