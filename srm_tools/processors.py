9287ab2ed7054ace6062ff896049ee7bafb4581d
Paul Walsh <paul@walsh.co.il>
2021-10-06 22:35:43 +0300

    Mahlakot Revaha integration.

---
+++ srm_tools/processors.py
@@ -0,0 +1,16 @@
+import dataflows as DF
+
+
+def ensure_fields(field_map, resources=None):
+    return [ensure_field(key, args, resources=resources) for key, args in field_map.items()]
+
+
+def ensure_field(name, args, resources=None):
+    args = {'source': args} if isinstance(args, str) else args
+    name, source, type, transform = (
+        name,
+        args.get('source', None),
+        args.get('type', 'string'),
+        args.get('transform', lambda r: r.get(source) if source else None),
+    )
+    return DF.add_field(name, type, transform, resources=resources)
