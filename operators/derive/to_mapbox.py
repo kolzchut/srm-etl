08bf4bffabe5c894b9f60c2ce46db5df151f6219
Adam Kariv <adam.kariv@gmail.com>
2021-11-09 19:52:13 +0200

    Reduce the amount of data in points

--- operators/derive/to_mapbox.py
+++ operators/derive/to_mapbox.py
@@ -152,6 +152,7 @@ def geo_data_flow():
 
         # Save mapbox data to ES and CKAN
         DF.update_resource('geo_data', name='points'),
+        DF.select_fields(['geometry', 'response_categories', 'point_id', 'response_ids', 'situation_ids', 'response_category']),
         DF.add_field('score', 'number', 10, resources=['points']),
         dump_to_es_and_delete(
             indexes=dict(srm__points=[dict(resource_name='points')]),
