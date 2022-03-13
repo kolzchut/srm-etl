a9e8f38f18b4219066eeaf1b2813a1503086ae8f
Adam Kariv <adam.kariv@gmail.com>
2022-03-13 14:50:03 +0200

    Set base zoom for points

--- operators/derive/to_mapbox.py
+++ operators/derive/to_mapbox.py
@@ -27,7 +27,7 @@ def upload_tileset(filename, tileset, name):
         mbtiles.unlink()
     mbtiles = str(mbtiles)
     layer_name = tileset.split('.')[-1].replace('-', '_')
-    cmd = ['tippecanoe', '-B5', '-z10', '-o', mbtiles, '-n', name, '-l', layer_name, filename]
+    cmd = ['tippecanoe', '-B6', '-z10', '-o', mbtiles, '-n', name, '-l', layer_name, filename]
     try:
         out = subprocess.check_output(cmd, stderr=subprocess.STDOUT).decode('utf8')
         print(out)
