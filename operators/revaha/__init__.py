27d58007d7e7b23493479fb0a6d71138ef17d67b
Adam Kariv <adam.kariv@gmail.com>
2023-11-07 16:34:25 +0200

    Adds emergency tag to revaha

--- operators/revaha/__init__.py
+++ operators/revaha/__init__.py
@@ -45,6 +45,8 @@ DATA_SOURCES = f'המידע מ<a target="_blank" href="{BASE_URL}" target="_blan
 PAYMENT_DETAILS = 'נדרש תיאום מראש'
 
 BRANCH_NAME_PREFIX = 'מחלקה לשירותים חברתיים'
+EMERGENCY_DESCRIPTION = '\n\n' + 'בזמן המלחמה המחלקה לשירותים חברתיים מעניקה סיוע, ייעוץ והכוונה גם למי שפונה ונמצא באזור המגורים בה פועלת המחלקה.'
+EMERGENCY_TAG = 'human_services:internal_emergency_services'
 
 ORGANIZATION = {
     'id': 'srm0020',
@@ -67,7 +69,7 @@ SERVICES = [
         'data': {
             'name': 'תמיכה וייעוץ ליחידים ומשפחות מטעם המחלקה לשירותים חברתיים',
             'source': DATA_SOURCE_ID,
-            'description': 'השירות מסייע לילדים, בני נוער, משפחות, יחידים, מוגבלים, זקנים, עולים חדשים ולכל פרט/קבוצה החפצים בקבלת סיוע. המחלקות לשירותים חברתיים  מעניקות מידע, יעוץ, טיפול, שירותים סוציאליים, הכוונה, תיווך לקבלת שירות, שילוב במסגרות ושירותי עזר בבית - בהתאם לכללי נזקקות וזכאות ולאפשרויות התקציביות.',
+            'description': 'השירות מסייע לילדים, בני נוער, משפחות, יחידים, מוגבלים, זקנים, עולים חדשים ולכל פרט/קבוצה החפצים בקבלת סיוע. המחלקות לשירותים חברתיים  מעניקות מידע, יעוץ, טיפול, שירותים סוציאליים, הכוונה, תיווך לקבלת שירות, שילוב במסגרות ושירותי עזר בבית - בהתאם לכללי נזקקות וזכאות ולאפשרויות התקציביות.' + EMERGENCY_DESCRIPTION,
             'payment_required': 'no',
             'urls': f'{BASE_URL}#{BRANCH_NAME_PREFIX}',
             'organizations': ['srm0020'],
@@ -77,6 +79,7 @@ SERVICES = [
                 'human_services:food',
                 'human_services:care',
                 'human_services:legal:advocacy_legal_aid:understand_government_programs',
+                EMERGENCY_TAG
             ],
             'situations': []
         }
@@ -86,7 +89,7 @@ SERVICES = [
         'data': {
             'name': 'סיוע לאזרחים ותיקים מטעם המחלקה לשירותים חברתיים',
             'source': DATA_SOURCE_ID,
-            'description': 'השירות לאוכלוסיית התושבים הוותיקים ובני משפחותיהם ניתן במחלקות לשירותים חברתיים וכולל מיצוי זכויות, מידע על מסגרות יומיות ,שירותי סעד, עובדים זרים, דיור מוגן ומסגרות מוסדיות זמניות וקבועות.',
+            'description': 'השירות לאוכלוסיית התושבים הוותיקים ובני משפחותיהם ניתן במחלקות לשירותים חברתיים וכולל מיצוי זכויות, מידע על מסגרות יומיות ,שירותי סעד, עובדים זרים, דיור מוגן ומסגרות מוסדיות זמניות וקבועות.' + EMERGENCY_DESCRIPTION,
             'payment_required': 'no',
             'urls': f'{BASE_URL}#{BRANCH_NAME_PREFIX}',
             'organizations': ['srm0020'],
@@ -94,6 +97,7 @@ SERVICES = [
             'data_sources': DATA_SOURCES,
             'responses': [
                 'human_services:care',
+                EMERGENCY_TAG
             ],
             'situations': [
                 'human_situations:age_group:seniors',
@@ -105,7 +109,7 @@ SERVICES = [
         'data': {
             'name': 'סיוע לאנשים עם מוגבלות מטעם המחלקה לשירותים חברתיים',
             'source': DATA_SOURCE_ID,
-            'description': 'השירות לאנשים  עם מוגבלות ובני משפחותיהם ניתן במחלקות לשירותים חברתיים ומיועד לאנשים עם פיגור שכלי, אוטיזם, מוגבלויות פיזיות וחושיות (עיוורון וחירשות) ולבני משפחותיהם. למימוש הזכאות לשירותים יש צורך בהכרה של משרד הרווחה והשירותים החברתיים.',
+            'description': 'השירות לאנשים  עם מוגבלות ובני משפחותיהם ניתן במחלקות לשירותים חברתיים ומיועד לאנשים עם פיגור שכלי, אוטיזם, מוגבלויות פיזיות וחושיות (עיוורון וחירשות) ולבני משפחותיהם. למימוש הזכאות לשירותים יש צורך בהכרה של משרד הרווחה והשירותים החברתיים.' + EMERGENCY_DESCRIPTION,
             'payment_required': 'no',
             'urls': f'{BASE_URL}#{BRANCH_NAME_PREFIX}',
             'organizations': ['srm0020'],
@@ -113,6 +117,7 @@ SERVICES = [
             'data_sources': DATA_SOURCES,
             'responses': [
                 'human_services:care',
+                EMERGENCY_TAG
             ],
             'situations': [
                 'human_situations:disability',
