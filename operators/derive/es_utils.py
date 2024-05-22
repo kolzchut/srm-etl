39b44eb5725c2345069ea945481a70ff68b9041a
Adam Kariv <adam.kariv@gmail.com>
2024-05-22 12:43:41 +0300

    Limit ES resettling timeout

--- operators/derive/es_utils.py
+++ operators/derive/es_utils.py
@@ -57,7 +57,7 @@ def dump_to_es_and_delete(**kwargs):
     kwargs.setdefault('mapper_cls', SRMMappingGenerator)
 
     def deleter():
-        time.sleep(300)
+        time.sleep(30) # wait for ES to settle
         for index in indexes:
             response = engine.delete_by_query(index, body=dict(query=dict(bool=dict(must_not=dict(term=dict(revision=unique_id))))), conflicts='proceed')
             print('DELETED', index, response)
