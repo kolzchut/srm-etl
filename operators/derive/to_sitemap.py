a51bd0d8b6b8f01b55d1aff20a7563d4e0354b6e
Adam Kariv <adam.kariv@gmail.com>
2024-05-27 14:02:36 +0300

    NUM_SITEMAPS

--- operators/derive/to_sitemap.py
+++ operators/derive/to_sitemap.py
@@ -8,6 +8,8 @@ from datapackage import Package
 from conf import settings
 from srm_tools.logger import logger
 
+NUM_SITEMAPS = 2
+
 def data_api_sitemap_flow():
     urls = DF.Flow(
         DF.load(f'{settings.DATA_DUMP_DIR}/autocomplete/datapackage.json'),
@@ -50,7 +52,7 @@ def data_api_sitemap_flow():
                 )
             ))
             idx += 1
-        assert len(resources) == 3, 'Expected 3 resources, got %d' % len(resources)
+        assert len(resources) == NUM_SITEMAPS, f'Expected {NUM_SITEMAPS} resources, got {len(resources)}'
         dumper = dump_to_ckan(settings.CKAN_HOST, settings.CKAN_API_KEY, settings.CKAN_OWNER_ORG, force_format=False)
         datapackage = dict(
             name='sitemap',
