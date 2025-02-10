b0be4629a622629350d3e4bba0e1bf9537fda247
Amir <amirb4@gmail.com>
2025-02-10 09:58:27 +0200

    debugging

--- operators/test_email_notifier/__init__.py
+++ operators/test_email_notifier/__init__.py
@@ -1,12 +1,16 @@
 from srm_tools.logger import logger
 from srm_tools.error_notifier import invoke_on
 
-
-def operator(*_):
+def raise_exception():
+    print('print Testing Email Notifier')
     logger.info('Testing Email Notifier')
     raise Exception('Testing Email Notifier')
+    
+
+def operator(*_):
+    invoke_on(raise_exception, 'email_notifier', True)
 
 
 
 if __name__ == '__main__':
-    invoke_on(lambda: operator(), 'email_notifier', True)
+    pass
