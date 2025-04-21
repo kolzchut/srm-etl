ee114e0d72030ebc0a1514d3001c6677b025782a
Amir <amirb4@gmail.com>
2025-04-21 14:51:42 +0300

    fix delete_by_query to match the new api format

--- operators/derive/es_utils.py
+++ operators/derive/es_utils.py
@@ -59,7 +59,11 @@ def dump_to_es_and_delete(**kwargs):
     def deleter():
         time.sleep(30) # wait for ES to settle
         for index in indexes:
-            response = engine.delete_by_query(index, body=dict(query=dict(bool=dict(must_not=dict(term=dict(revision=unique_id))))), conflicts='proceed')
+            response = engine.delete_by_query(
+                index=index, 
+                query=dict(bool=dict(must_not=dict(term=dict(revision=unique_id)))),
+                conflicts='proceed'
+            )
             print('DELETED', index, response)
 
     return DF.Flow(
