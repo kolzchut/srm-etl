60c0e665f56412694301f0f714290daeae755fe4
arielrepos <ariel_ohana@webiks.com>
2025-06-08 09:12:03 +0300

    Added retry to ES instance

--- operators/derive/es_utils.py
+++ operators/derive/es_utils.py
@@ -13,7 +13,7 @@ from conf import settings
 def es_instance():
     return elasticsearch.Elasticsearch(
         [dict(host=settings.ES_HOST, port=int(settings.ES_PORT))],
-        timeout=60,
+        timeout=60, retry_on_timeout=True, max_retries=3, retry_on_status=[502, 503, 504],
         **({"http_auth": settings.ES_HTTP_AUTH.split(':')} if settings.ES_HTTP_AUTH else {}),
     )
 
