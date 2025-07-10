2f9656b045d5b7bccdf4cf7d1525e7212d1290e8
arielrepos <ariel_ohana@webiks.com>
2025-07-10 14:44:23 +0300

    Revert "Tried to set [] so it will have a fallback to avoid falling out of the safe lambda"
    This reverts commit 82888e3c

--- operators/derive/to_dp.py
+++ operators/derive/to_dp.py
@@ -693,22 +693,22 @@ def card_data_flow():
         try:
             return func(*args, **kwargs)
         except Exception as e:
-            warning = f"ERROR BUT CONTINUE: Fallback 'or' used in arguments: {args}"
+            warning = f"Fallback BUT CONTINUE: {e}"
             logger.warning(warning)
-            send_failure_email(operation_name="Upload To DB - DP process", error=warning)
+            send_failure_email(operation_name="Upload To DB - DP process", error=warning, reraise=False)
             return default
 
 
     return DF.Flow(
         DF.checkpoint(CHECKPOINT),
-        DF.add_field('situations', 'array', lambda r: safe_lambda(lambda r: [situations[s] for s in (r.get('situation_ids') or []) if s in situations], r, default=[]), resources=['card_data']),
-        DF.add_field('responses', 'array', lambda r: safe_lambda(lambda r: [responses[s] for s in (r.get('response_ids') or []) if s in responses], r, default=[]), resources=['card_data']),
+        DF.add_field('situations', 'array', lambda r: safe_lambda(lambda r: [situations[s] for s in (r.get('situation_ids')) if s in situations], r, default=[]), resources=['card_data']),
+        DF.add_field('responses', 'array', lambda r: safe_lambda(lambda r: [responses[s] for s in (r.get('response_ids')) if s in responses], r, default=[]), resources=['card_data']),
         rs_score.process('card_data'),
         DF.add_field('situation_ids_parents', 'array', lambda r: safe_lambda(helpers.update_taxonomy_with_parents, r.get('situation_ids', []), default=[]), resources=['card_data']),
         DF.add_field('response_ids_parents', 'array', lambda r: safe_lambda(helpers.update_taxonomy_with_parents, r.get('response_ids', []), default=[]), resources=['card_data']),
         DF.delete_fields(['service_situations', 'branch_situations', 'organization_situations', 'service_responses', 'auto_tagged'], resources=['card_data']),
-        DF.add_field('situations_parents', 'array', lambda r: safe_lambda(lambda r: [situations[s] for s in (r.get('situation_ids_parents') or []) if s in situations], r, default=[]), resources=['card_data']),
-        DF.add_field('responses_parents', 'array', lambda r: safe_lambda(lambda r: [responses[s] for s in (r.get('response_ids_parents') or []) if s in responses], r, default=[]), resources=['card_data']),
+        DF.add_field('situations_parents', 'array', lambda r: safe_lambda(lambda r: [situations[s] for s in (r.get('situation_ids_parents')) if s in situations], r, default=[]), resources=['card_data']),
+        DF.add_field('responses_parents', 'array', lambda r: safe_lambda(lambda r: [responses[s] for s in (r.get('response_ids_parents')) if s in responses], r, default=[]), resources=['card_data']),
         DF.set_type('situation_ids', **KEYWORD_STRING, resources=['card_data']),
         DF.set_type('response_ids', **KEYWORD_STRING, resources=['card_data']),
         DF.set_type('situation_ids_parents', **KEYWORD_STRING, resources=['card_data']),
