5f7075900d4c00bae8f4b5338ed462243bd705c6
arielrepos <ariel_ohana@webiks.com>
2025-07-15 09:00:52 +0300

    Checking what is normalize Ids

--- operators/derive/manual_fixes.py
+++ operators/derive/manual_fixes.py
@@ -32,7 +32,9 @@ class ManualFixes():
 
     def normalize_ids(self, slugs):
         slugs = slugs or ''
-        return ','.join(sorted(filter(None, set(s.strip() for s in slugs.split(',')))))
+        normalizeIds = ','.join(sorted(filter(None, set(s.strip() for s in slugs.split(',')))))
+        logger.log(f'Normalizing IDs: {slugs} -> {normalizeIds}')
+        return
 
     def apply_manual_fixes(self):
         def func(row):
