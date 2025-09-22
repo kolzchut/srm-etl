a60c575f67f355572ae2d9c8b8f1b4506e670ba6
arielrepos <ariel_ohana@webiks.com>
2025-09-22 12:12:27 +0300

    Testing

--- operators/meser/__init__.py
+++ operators/meser/__init__.py
@@ -202,7 +202,7 @@ def run(*_):
             'entities', ['id'],
             DF.Flow(
                 DF.load(os.path.join(dirname, 'meser', 'denormalized', 'datapackage.json')),
-                DF.join_with_self('meser', ['organization_id'], fields=dict(organization_id=None)),
+                DF.join_with_self('meser', ['organization_id'], fields=dict(organization_id=None, meser_id=None,)),
                 DF.rename_fields({'organization_id': 'id'}, resources='meser'),
                 DF.add_field('data', 'object', lambda r: dict(id=r['id'])),
                 DF.printer()
