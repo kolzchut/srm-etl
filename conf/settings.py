e96f4d3a3c0e61a92aa19d1771bab88f04f1b001
arielrepos <ariel_ohana@webiks.com>
2026-01-06 15:31:24 +0200

    Changed Base for staging

--- conf/settings.py
+++ conf/settings.py
@@ -26,11 +26,11 @@ GOVMAP_AUTH = 'https://ags.govmap.gov.il/Api/Controllers/GovmapApi/Auth'
 GOVMAP_REQUEST_ORIGIN = 'https://www.kolzchut.org.il'
 GOVMAP_GEOCODE_API = 'https://ags.govmap.gov.il/Api/Controllers/GovmapApi/Geocode'
 
-AIRTABLE_BASE ="appGNajPmotY4U8ny" #  Replaced for testing purposes only get_env('ETL_AIRTABLE_BASE')
+AIRTABLE_BASE ="appiJqrVjke9ppPaE" #  Replaced for testing purposes only get_env('ETL_AIRTABLE_BASE')
 AIRTABLE_ALTERNATE_BASE = get_env('ETL_AIRTABLE_ALTERNATE_BASE')
 AIRTABLE_DATAENTRY_BASE = get_env('ETL_AIRTABLE_DATAENTRY_BASE')
 AIRTABLE_DATA_IMPORT_BASE = get_env('ETL_AIRTABLE_DATA_IMPORT_BASE')
-AIRTABLE_STAGING_BASE = "appGNajPmotY4U8ny" # Replaced for testing purposes only "appxUxbrRRoUiSB4P"
+AIRTABLE_STAGING_BASE = "appiJqrVjke9ppPaE" # Replaced for testing purposes only "appxUxbrRRoUiSB4P"
 
 AIRTABLE_VIEW = 'Grid view'
 AIRTABLE_LOCATION_TABLE = 'Locations'
